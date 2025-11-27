8b948959952cca1386ae5549de270b1c6d9a4a06
Sam-kiran <samanvaikiran19@gmail.com>
2025-11-27 15:34:08 +0530

    Fix EMAIL_PORT environment variable handling

--- campusnexus/settings.py
+++ campusnexus/settings.py
@@ -232,10 +232,10 @@ else:
 # Email Configuration
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT') or '587')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') or ''
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') or ''
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@campusnexus.com')
 
 # Gmail Pub/Sub / Gmail watch settings
